7a14f448f19b6dbc4e5acef7a04b0be468de3fd0
lysbaldrez <lyscalefi@gmail.com>
2020-05-20 22:38:09 -0400

    Adicionada função de calculo de imc e adição na tabela.

--- js/principal.js
+++ js/principal.js
@@ -40,3 +40,51 @@ for (var i = 0; i < pacientes.length; i++) {
     tdImc.textContent = imc.toFixed(2);
   }
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  var form = document.querySelector("#form-adiciona");
+
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr");
+
+  var nomeTd = document.createElement("td");
+  var pesoTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+  imcTd.textContent = (peso / (altura * altura)).toFixed(2);
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(imcTd);
+
+  var tabela = document.querySelector("#tabela-pacientes");
+
+  tabela.appendChild(pacienteTr);
+
+
+});
+
+titulo.addEventListener("click", function () {
+
+
+}); // funçaõ anônima 
+
+
+// function mostraMensagem() {
+//  console.log("Olá eu fui clicado!")
+//} exemplo de função nomeada
